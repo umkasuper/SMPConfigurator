4eaaf4c5986c44e4388a49dbe8c3f5061e5b4659
Maksim Kolesnikov <maksim@m-200.com>
2014-04-07 21:35:13 +0400

    Первые изменения

--- src/SMPConfigurator/SMPConfigurator.java
+++ src/SMPConfigurator/SMPConfigurator.java
@@ -1,4 +1,14 @@
 package SMPConfigurator;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
+import static java.lang.System.*;
+
 public class SMPConfigurator {
+    public MenuItem close;
+
+    public void OnClose(ActionEvent actionEvent) {
+        out.print("On close");
+    }
 }
